e89695584ff7c5f934aa042b652335f6ae11ebd0
Alejandro Sandí <alesf22@gmail.com>
2024-05-29 09:59:31 -0600

    feat: add throttler and mailer configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,53 @@
-import { Module } from '@nestjs/common';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { forwardRef, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { AnalyticsModule } from './core/analytics/analytics.module';
+import { AuthModule } from './core/auth/auth.module';
+import { HealthModule } from './core/health/health.module';
+import { URLsModule } from './core/urls/urls.module';
+import { UsersModule } from './core/users/users.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 20,
+      },
+    ]),
+    MailerModule.forRootAsync({
+      useFactory: () => ({
+        transport: {
+          host: process.env.MAIL_HOST,
+          port: process.env.MAIL_PORT,
+          secure: false,
+          auth: {
+            user: process.env.MAIL_USER,
+            pass: process.env.MAIL_PASS,
+          },
+        },
+        defaults: {
+          from: `Shrinkr <${process.env.MAIL_FROM}>`,
+        },
+      }),
+    }),
+    DatabaseModule,
+    UsersModule,
+    URLsModule,
+    AnalyticsModule,
+    forwardRef(() => AuthModule),
+    forwardRef(() => HealthModule),
+  ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
